b634455c5acef25585ad912658fcf36814b6e605
[Kazim Rizvi] <kazimrizvi1999@gmail.com>
2023-05-15 01:24:34 +0500

    Target by set

---
+++ Target(set).py
@@ -0,0 +1,9 @@
+set={2,4,6,8,10}
+Target=10
+for i in set:
+    num=Target-i
+    if num in set:
+        print(num)
+
+
+
---
+++ secondMax_Num.py
@@ -0,0 +1,16 @@
+arr=[1,78,93,24,0]
+n=len(arr)
+Fmax=arr[0]
+
+for i in range(1,n):
+    if arr[i]>Fmax:
+        Fmax=arr[i]
+    
+        
+    
+
+
+   
+
+
+        
